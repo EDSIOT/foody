cea49789516705f9c45fd887ce783b161d852592
clement <68778682+EDSIOT@users.noreply.github.com>
2026-08-14 22:31:48 +0200

    fix: online api connection

--- frontend/composables/useApi.js
+++ frontend/composables/useApi.js
@@ -18,9 +18,21 @@ export const useApi = () => {
     return await $fetch(`${base}/regions/top3`, { params: { days } })
   }
 
-  const getCuisinesList = async () => {
+/*  const getCuisinesList = async () => {
     return await $fetch(`${base}/cuisines/list`)
-  }
+  } */
+    const getCuisinesList = async () => {
+  const response = await fetch(`${base}/cuisines/list`)
+
+  console.log('STATUS:', response.status)
+  console.log('CONTENT-TYPE:', response.headers.get('content-type'))
+
+  const text = await response.text()
+
+  console.log('RAW RESPONSE:', text)
+
+  return JSON.parse(text)
+}
 
   const getRegionScoresForCuisine = async (cuisine, days = 7) => {
     return await $fetch(`${base}/regions/by-cuisine`, { params: { cuisine, days } })
