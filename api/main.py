e37d1cb20672e48dfcf01398c9665b2acabad9c4
clement <68778682+EDSIOT@users.noreply.github.com>
2026-08-14 22:40:08 +0200

    fix; vercel conection

--- api/main.py
+++ api/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Foody API", version="0.1.0")
 # CORS pour autoriser ton futur frontend Nuxt (en dev, sur localhost:3000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # à restreindre en prod
+    allow_origins=["http://localhost:3000","https://foody-one-wine.vercel.app"],  # à restreindre en prod
     allow_methods=["GET"],
     allow_headers=["*"],
 )
